04ae7a6877d2b3c9a549349c3784cf33d40b3119
Oscar Ayuso <oscar.ayuso@epitech.eu>
2019-03-25 10:09:31 +0100

    finally

---
+++ sdl2/Sdl.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_arcade_2018
+** File description:
+** Sdl
+*/
+
+#include "Sdl.hpp"
+
+Sdl::Sdl()
+{
+}
+
+Sdl::~Sdl()
+{
+}
---
+++ sdl2/Sdl.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2019
+** OOP_arcade_2018
+** File description:
+** Sdl
+*/
+
+#ifndef SDL_HPP_
+    #define SDL_HPP_
+
+class Sdl {
+	public:
+		Sdl();
+		~Sdl();
+
+	protected:
+	private:
+};
+
+#endif /* !SDL_HPP_ */
